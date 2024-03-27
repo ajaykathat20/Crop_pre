2ae9bd8061f9176e2e690eb18a2d4a18399362f2
ajaykathat20 <ajay.19jdds002@jietjodhpur.ac.in>
2024-03-28 04:05:50 +0530

    Crop_yield

--- crop_app.py
+++ crop_app.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 pickle_in = open(r"E:\Crop yieldss Prediction\Crop_model.pkl","rb")
 Crop_model=pickle.load(pickle_in)
+dtr = pickle.load(open(r'E:\Crop yieldss Prediction\model.pkl','rb'))
+preprocessor = pickle.load(open(r'E:\Crop yieldss Prediction\pre.pkl','rb'))
 
 
 @app.route('/')
@@ -35,8 +37,26 @@ def brain():
         return render_template('prediction.html', prediction=str(acc))
     else:
         return "Sorry...  Error in entered values in the form Please check the values and fill it again"
+@app.route('/yield')
+def crop_yield():
+    return render_template('crop_yield.html')
 
 
+@app.route("/predict_yield",methods=['POST'])
+def predict_yield():
+    if request.method == 'POST':
+        Year = request.form['Year']
+        average_rain_fall_mm_per_year = request.form['average_rain_fall_mm_per_year']
+        pesticides_tonnes = request.form['pesticides_tonnes']
+        avg_temp = request.form['avg_temp']
+        Area = request.form['Area']
+        Item  = request.form['Item']
+
+        features =[[Year,average_rain_fall_mm_per_year,pesticides_tonnes,avg_temp,Area,Item]]
+        transformed_features = preprocessor.transform(features)
+        prediction = dtr.predict(transformed_features)
+
+        return render_template('crop_yield.html',prediction = prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
